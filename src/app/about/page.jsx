e90c352acf5682cc0d896b9402d2574f5e4c70ac
bowasia <leejingsawat@gmail.com>
2024-02-13 13:51:09 +0700

    testing route

---
+++ src/app/about/page.jsx
@@ -0,0 +1,11 @@
+import Link from 'next/link'
+import React from 'react'
+
+export default function aboutPage() {
+  return (
+    <>
+      <div>This is aboutPage 2</div>
+      <Link href='/'> Back to first page</Link>
+    </>
+  )
+}
