3d56b23476a55c3bfe7c8a8b655c252a10e5cec5
Bowasia <leejingsawat@gmail.com>
2024-02-18 22:33:18 +0700

    fix GetLIFF typo

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import Nav from "./components/Nav";
-import GetLIFF from "./components/getLIFF";
+import GetLIFF from "./components/GetLIFF";
 
 
 export default function Home() {
