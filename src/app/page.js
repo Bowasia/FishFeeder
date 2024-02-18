66298ec7b935400492560eb7ff1bca2efbc5587d
Bowasia <leejingsawat@gmail.com>
2024-02-18 23:41:41 +0700

    fix file naming bug

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
-import Nav from "./components/Nav";
-import GetLIFF from "./components/GetLIFF";
+import Nav from "./component/Nav";
+import GetLIFF from "./component/GetLIFF";
 
 
 export default function Home() {
