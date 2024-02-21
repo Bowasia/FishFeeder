eda55a9276bd1cd819555623f8dc7492f8ce8747
Bowasia <leejingsawat@gmail.com>
2024-02-21 18:00:27 +0700

    payment trial00

--- src/app/components/Payment.jsx
+++ src/app/components/Payment.jsx
@@ -1,95 +1,98 @@
-"use client"
-import React from 'react'
-import { nanoid } from 'nanoid';
+// "use client"
+// import React from 'react'
+// import { nanoid } from 'nanoid';
 
-export default function Payment({}) {
+// const uniqueNonce = Date.now().toString() + nanoid()
+// const crypto = require('crypto')
+// const channelId = '2003139835'; // Replace with your Channel ID
+// const channelSecretKey = '2fc3ff5d8d8652e6c06623829b4fe92e'; // Replace with your Channel Secret Key
+// const baseUrl = 'https://sandbox-api-pay.line.me';
+// const requestUrl = '/v3/payments/request';
+// const headers = {
+//   'Content-Type': 'application/json',
+//   'X-LINE-ChannelId': channelId,
+//   'X-LINE-Authorization-Nonce': uniqueNonce, // Replace with a unique nonce value
+//   'X-LINE-Authorization': generateHmacSignature(body, channelSecretKey), // Replace with the HMAC signature
+// };
 
-    const generateHmacSignature = (data, channelSecretKey) => {
-        const hmac = crypto.createHmac('sha256', channelSecretKey);
-        hmac.update(data)
-        return hmac.digest('hex')
-    }
+// export default function Payment() {
 
-    // Sample code for Request Payment API
-const requestPayment = async () => {
-    const uniqueNonce = Date.now().toString() + nanoid()
-    const crypto = require('crypto')
-    const channelId = '2003139835'; // Replace with your Channel ID
-    const channelSecretKey = '2fc3ff5d8d8652e6c06623829b4fe92e'; // Replace with your Channel Secret Key
-    const requestUrl = 'https://sandbox-api-pay.line.me/v3/payments/request';
-    const headers = {
-      'Content-Type': 'application/json',
-      'X-LINE-ChannelId': channelId,
-      'X-LINE-Authorization-Nonce': uniqueNonce, // Replace with a unique nonce value
-      'X-LINE-Authorization': generateHmacSignature(body, channelSecretKey), // Replace with the HMAC signature
-    };
-    const body = {
-      amount: 15,
-      currency: 'THB',
-      orderId: '1', 
-      packages: [
-        {
-          id: '1', 
-          amount: 1,
-          name: 'Package name',
-          products: [
-            {
-              name: 'Fish Feeder Pass',
-              quantity: 1,
-              price: 15,
-            },
-          ],
-        },
-      ],
-      redirectUrls: {
-        confirmUrl: 'http://localhost:3000/about', // Replace with your confirm URL
-        cancelUrl: 'http://localhost:3000/', // Replace with your cancel URL
-      },
-    };
+//     const generateHmacSignature = (data, channelSecretKey) => {
+//         const hmac = crypto.createHmac('sha256', channelSecretKey);
+//         hmac.update(data)
+//         return hmac.digest('hex')
+//     }
+
+//     // Sample code for Request Payment API
+// const requestPayment = async () => {
+    
+//     const body = {
+//       amount: 15,
+//       currency: 'THB',
+//       orderId: '1', 
+//       packages: [
+//         {
+//           id: '01A', 
+//           amount: 1,
+//           name: 'Package name',
+//           products: [
+//             {
+//               name: 'Fish Feeder Pass',
+//               quantity: 1,
+//               price: 15,
+//             },
+//           ],
+//         },
+//       ],
+//       redirectUrls: {
+//         confirmUrl: 'http://localhost:3000/about', // Replace with your confirm URL
+//         cancelUrl: 'http://localhost:3000/', // Replace with your cancel URL
+//       },
+//     };
   
-    try {
-      const response = await fetch(requestUrl, {
-        method: 'POST',
-        headers: headers,
-        body: JSON.stringify(body),
-      });
-      const responseData = await response.json();
-      console.log(responseData);
-    } catch (error) {
-      console.error(error);
-    }
-  };
+//     try {
+//       const response = await fetch(requestUrl, {
+//         method: 'POST',
+//         headers: headers,
+//         body: JSON.stringify(body),
+//       });
+//       const responseData = await response.json();
+//       console.log(responseData);
+//     } catch (error) {
+//       console.error(error);
+//     }
+//   };
   
-  // // Sample code for Confirm API
-  // const confirmPayment = async (transactionId) => {
-  //   const channelId = '2003139835'; // Replace with your Channel ID
-  //   const channelSecretKey = '2fc3ff5d8d8652e6c06623829b4fe92e'; // Replace with your Channel Secret Key
-  //   const confirmUrl = `https://sandbox-api-pay.line.me/v3/payments/${transactionId}/confirm`;
-  //   const headers = {
-  //     'Content-Type': 'application/json',
-  //     'X-LINE-ChannelId': channelId,
-  //     'X-LINE-Authorization-Nonce': 'UNIQUE_NONCE', // Replace with a unique nonce value
-  //     'X-LINE-Authorization': 'GENERATED_SIGNATURE', // Replace with the HMAC signature
-  //   };
-  //   const body = {
-  //     amount: 1000,
-  //     currency: 'JPY',
-  //   };
+//   // // Sample code for Confirm API
+//   // const confirmPayment = async (transactionId) => {
+//   //   const channelId = '2003139835'; // Replace with your Channel ID
+//   //   const channelSecretKey = '2fc3ff5d8d8652e6c06623829b4fe92e'; // Replace with your Channel Secret Key
+//   //   const confirmUrl = `https://sandbox-api-pay.line.me/v3/payments/${transactionId}/confirm`;
+//   //   const headers = {
+//   //     'Content-Type': 'application/json',
+//   //     'X-LINE-ChannelId': channelId,
+//   //     'X-LINE-Authorization-Nonce': 'UNIQUE_NONCE', // Replace with a unique nonce value
+//   //     'X-LINE-Authorization': 'GENERATED_SIGNATURE', // Replace with the HMAC signature
+//   //   };
+//   //   const body = {
+//   //     amount: 1000,
+//   //     currency: 'JPY',
+//   //   };
   
-  //   try {
-  //     const response = await fetch(confirmUrl, {
-  //       method: 'POST',
-  //       headers: headers,
-  //       body: JSON.stringify(body),
-  //     });
-  //     const responseData = await response.json();
-  //     console.log(responseData);
-  //   } catch (error) {
-  //     console.error(error);
-  //   }
-  // };
+//   //   try {
+//   //     const response = await fetch(confirmUrl, {
+//   //       method: 'POST',
+//   //       headers: headers,
+//   //       body: JSON.stringify(body),
+//   //     });
+//   //     const responseData = await response.json();
+//   //     console.log(responseData);
+//   //   } catch (error) {
+//   //     console.error(error);
+//   //   }
+//   // };
   
-  return (
-    <button onClick={() => {requestPayment()}}> Make transaction </button>
-  )
-}
+//   return (
+//     <button onClick={() => {requestPayment()}}> Make transaction </button>
+//   )
+// }
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,17 +1,92 @@
+'use client'
 import Image from "next/image";
 import Link from "next/link";
 import Nav from "./components/Nav";
 import GetLIFF from "./components/GetLIFF";
 import Payment from "./components/Payment";
 
+import { nanoid } from "nanoid";
+
+const crypto = require('crypto')
+const ChannelID = '2003139835'; // Replace with your Channel ID
+const ChannelSecretKey = '2fc3ff5d8d8652e6c06623829b4fe92e'; // Replace with your Channel Secret Key
+const baseUrl = 'https://sandbox-api-pay.line.me';
+const pathUrl = '/v3/payments/request'
+
+const requestUrl = baseUrl + pathUrl;
+const uniqueNonce = () => Date.now().toString() + nanoid()
+// const headers = {
+//   'Content-Type': 'application/json',
+//   'X-LINE-ChannelId': ChannelID,
+//   'X-LINE-Authorization-Nonce': uniqueNonce, // Replace with a unique nonce value
+//   'X-LINE-Authorization': generateHmacSignature(body, ChannelSecretKey), // Replace with the HMAC signature
+// };
+
+
 
 export default function Home() {
+
+  const genHeader = (ChannelID, ChannelSecret, URL, RequestDetail, nonce) => {
+    if (typeof RequestDetail !== "string") {
+      RequestDetail = JSON.stringify(RequestDetail);
+    }
+    const data = ChannelSecret + URL + RequestDetail + nonce;
+    const signature = crypto.createHmac("SHA256", ChannelSecret).update(data).digest("base64").toString();
+    return {
+      "Content-Type": "application/json",
+      "X-LINE-ChannelId": ChannelID,
+      "X-LINE-Authorization-Nonce": nonce,
+      "X-LINE-Authorization": signature
+    };
+  };
+
+  const RequestBody = {
+    amount: 15,
+      currency: 'THB',
+      orderId: '1', 
+      packages: [
+        {
+          id: '01A', 
+          amount: 1,
+          name: 'Package name',
+          products: [
+            {
+              name: 'Fish Feeder Pass',
+              quantity: 1,
+              price: 15,
+            },
+          ],
+        },
+      ],
+      redirectUrls: {
+        confirmUrl: 'http://localhost:3000/about', // Replace with your confirm URL
+        cancelUrl: 'http://localhost:3000/', // Replace with your cancel URL
+      },
+  }
+
+  const handleClick = async () => {
+    var header = genHeader(ChannelId, ChannelSecret, pathUrl, RequestBody, uniqueNonce());
+    try {
+      const response = await fetch(requestUrl,
+        {
+          method: 'POST',
+          headers: header,
+          body: JSON.stringify(RequestBody)
+        })
+      const responseData = await response.json()
+      console.log(responseData)
+    } catch (error) {
+      console.log(error)
+    }
+    
+  } 
 
   return (
     <>
       <Nav />
       <GetLIFF />
-      <Payment />
+      <button onClick={handleClick}>Make purchase</button>
+      {/* <Payment /> */}
     </>
   );
 }
